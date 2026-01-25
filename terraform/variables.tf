70532bc4d69fc1fe9b113b4a4935f59058644f70
gawakawa <mashurakiryu@icloud.com>
2026-01-25 20:44:36 +0900

    :bricks: Initialize Google Cloud Terraform configuration
    - Configure hashicorp/google provider v7.16
    - Set up project variables (project_id, region, zone)
    - Initialize OpenTofu with google provider

---
+++ terraform/variables.tf
@@ -0,0 +1,17 @@
+variable "project_id" {
+  description = "GCP project ID"
+  type        = string
+  default     = "yorimichi-map-485411"
+}
+
+variable "region" {
+  description = "GCP region"
+  type        = string
+  default     = "asia-northeast1"
+}
+
+variable "zone" {
+  description = "GCP zone"
+  type        = string
+  default     = "asia-northeast1-a"
+}
